22d92c744a51240f6f4dd73ce1ce77619720f219
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-06-21 17:51:37 +0300

    feat: ellipsis menu button opens context menu

--- src/Sticky/NoteControl.xaml.cs
+++ src/Sticky/NoteControl.xaml.cs
@@ -1,13 +1,15 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
 namespace Sticky {
 
   public partial class NoteControl : UserControl {
     public NoteControl() {
-      InitializeComponent();
       DataContextChanged += OnDataContextChanged;
+      InitializeComponent();
+      // @TODO: Handle note theme changes.
     }
 
     private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
@@ -19,6 +21,20 @@ namespace Sticky {
 
       Resources.Clear();
       Resources.MergedDictionaries.Add(theme);
+    }
+
+    private void OnOpenContextMenu(object sender, ExecutedRoutedEventArgs e) {
+      System.Console.WriteLine("OnOpenContextMenu");
+
+      // @TODO: Something weird happens here. The bindings used
+      // in the context menu do not work before the note control
+      // is left clicked once. That is, at least one of the note
+      // controls is clicked. After that the bindings work for
+      // all note controls... ???
+      var cm = ContextMenuService.GetContextMenu(NoteRichTextBox);
+      cm.PlacementTarget = NoteRichTextBox;
+      cm.Placement = PlacementMode.Center;
+      cm.IsOpen = true;
     }
   }
 
