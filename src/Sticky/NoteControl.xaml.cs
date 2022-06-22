df62ad36889f517de9a5c29ae00a5ad700f19955
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-06-22 09:57:44 +0300

    fix: context menu seems to work now, not ideal solution though...

--- src/Sticky/NoteControl.xaml.cs
+++ src/Sticky/NoteControl.xaml.cs
@@ -1,18 +1,30 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace Sticky {
 
   public partial class NoteControl : UserControl {
     public NoteControl() {
-      DataContextChanged += OnDataContextChanged;
+      DataContextChanged += (sender, e) => {
+        RefreshTheme();
+
+        // @TODO: Remove event handler.
+        var note = (NoteViewModel)DataContext;
+        note.PropertyChanged += (s, e) => {
+          if (e.PropertyName == "Theme") {
+            RefreshTheme();
+          }
+        };
+      };
+
       InitializeComponent();
       // @TODO: Handle note theme changes.
     }
 
-    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+    private void RefreshTheme() {
       var note = DataContext as NoteViewModel;
       if (note == null) return;
 
@@ -24,11 +36,9 @@ namespace Sticky {
     }
 
     private void OnOpenContextMenu(object sender, ExecutedRoutedEventArgs e) {
-      // @TODO: Something weird happens here. The bindings used
-      // in the context menu do not work before the note control
-      // is left clicked once. That is, at least one of the note
-      // controls is clicked. After that the bindings work for
-      // all note controls... ???
+      // @TODO: Context menu won't work properly without focus?
+      NoteRichTextBox.Focus();
+
       var cm = ContextMenuService.GetContextMenu(NoteRichTextBox);
       cm.PlacementTarget = NoteRichTextBox;
       cm.Placement = PlacementMode.Center;
