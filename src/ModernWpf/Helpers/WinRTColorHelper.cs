9168501ff05fcac07a849e23379655e2526adb15
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-06-29 15:01:27 +0300

    chore: WinRT dependency

---
+++ src/ModernWpf/Helpers/WinRTColorHelper.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace ModernWpf {
+  internal static class WinRTColorHelper {
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static Color ToColor(this Windows.UI.Color color) {
+      return Color.FromArgb(color.A, color.R, color.G, color.B);
+    }
+  }
+}
--- src/ModernWpf/UISettingsResources.cs
+++ src/ModernWpf/UISettingsResources.cs
@@ -2,10 +2,8 @@ using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Threading;
 using Microsoft.Win32;
-#if false // @TODO
 using Windows.Foundation.Metadata;
 using Windows.UI.ViewManagement;
-#endif
 
 namespace ModernWpf {
   internal class UISettingsResources : ResourceDictionary {
@@ -14,9 +12,7 @@ namespace ModernWpf {
 
     private readonly Dispatcher _dispatcher = Dispatcher.CurrentDispatcher;
 
-#if false // @TODO
     private UISettings _uiSettings;
-#endif
 
     public UISettingsResources() {
       if (DesignMode.DesignModeEnabled) {
@@ -30,7 +26,6 @@ namespace ModernWpf {
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private void Initialize() {
-#if false // @TODO
       _uiSettings = new UISettings();
 
       if (ApiInformation.IsApiContractPresent(UniversalApiContractName, 4)) {
@@ -40,12 +35,10 @@ namespace ModernWpf {
       if (ApiInformation.IsApiContractPresent(UniversalApiContractName, 8)) {
         InitializeForContract8();
       }
-#endif
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private void InitializeForContract4() {
-#if false // @TODO
       _uiSettings.AdvancedEffectsEnabledChanged += (sender, args) => {
         _dispatcher.BeginInvoke(ApplyAdvancedEffectsEnabled);
       };
@@ -59,36 +52,29 @@ namespace ModernWpf {
       }
 
       ApplyAdvancedEffectsEnabled();
-#endif
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private void InitializeForContract8() {
-#if false // @TODO
       _uiSettings.AutoHideScrollBarsChanged += (sender, args) => {
         _dispatcher.BeginInvoke(ApplyAutoHideScrollBars);
       };
       ApplyAutoHideScrollBars();
-#endif
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private void ApplyAdvancedEffectsEnabled() {
-#if false // @TODO
       var key = SystemParameters.DropShadowKey;
       if (_uiSettings.AdvancedEffectsEnabled) {
         Remove(key);
       } else {
         this[key] = false;
       }
-#endif
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private void ApplyAutoHideScrollBars() {
-#if false // @TODO
       this[AutoHideScrollBarsKey] = _uiSettings.AutoHideScrollBars;
-#endif
     }
   }
 }
