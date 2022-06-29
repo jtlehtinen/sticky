ac779f9aa9aa773225b1fe0b13801b0a1dac5596
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-06-29 18:04:26 +0300

    refactor: use ModernWpf's BindingProxy

--- src/Sticky/Converters.cs
+++ src/Sticky/Converters.cs
@@ -7,24 +7,6 @@ using System.Windows.Media;
 
 namespace Sticky {
 
-  // @TODO: Move to some other file...
-  // @NOTE: Only reason this exists to be able to use value
-  // converters with dynamic resources.
-  //
-  // https://thomaslevesque.com/2011/03/21/wpf-how-to-bind-to-data-when-the-datacontext-is-not-inherited/
-  public class BindingProxy : Freezable {
-    protected override Freezable CreateInstanceCore() {
-      return new BindingProxy();
-    }
-
-    public object Data {
-      get { return (object)GetValue(DataProperty); }
-      set { SetValue(DataProperty, value); }
-    }
-
-    public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
-  }
-
   public class BooleanToGeometryCombineModeConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
       var bvalue = (bool)value;
