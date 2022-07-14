7fe5d2f13a0aa7b0d2ba6fdb697d5e2e88d5e643
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-07-14 23:00:11 +0300

    feat: third party notices includes a link to each open source project that is opened in the default browser when clicked

--- src/Sticky/Pages/ThirdPartyNoticesPage.xaml.cs
+++ src/Sticky/Pages/ThirdPartyNoticesPage.xaml.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 using Sticky.ViewModels;
 
 namespace Sticky {
@@ -16,6 +18,14 @@ namespace Sticky {
 
     private void OnBack(object sender, RoutedEventArgs e) {
       App.Current.MainWindow.Navigate(PageType.Settings);
+    }
+
+    private void OnOpenBrowser(object sender, RequestNavigateEventArgs e) {
+      var info = new ProcessStartInfo(e.Uri.AbsoluteUri);
+      info.UseShellExecute = true;
+
+      Process.Start(info);
+      e.Handled = true;
     }
   }
 
